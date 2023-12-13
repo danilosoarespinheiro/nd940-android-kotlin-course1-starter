fa8b393ab10ffff0ab11f60eb4d4aa996c835294
danilosoarespinheiro <danilosoarespinheiro@gmail.com>
2023-12-13 00:28:05 -0400

    - Added OnboardingFragment.kt, InstructionsFragment.kt, ShoeListFragment.kt with the respective layouts
    - Added actions to fragments
    
    - Stopped at step 9

--- starter/app/src/main/java/com/udacity/shoestore/onboarding/InstructionsFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/onboarding/InstructionsFragment.kt
@@ -8,18 +8,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
-import com.udacity.shoestore.databinding.FragmentOnboardingBinding
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
-class OnboardingFragment : Fragment() {
+class InstructionsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding: FragmentOnboardingBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_onboarding, container, false)
+        val binding: FragmentInstructionsBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
 
         binding.buttonProceed.setOnClickListener {
-            findNavController().navigate(OnboardingFragmentDirections.actionOnboardingFragmentToInstructionsFragment())
+            findNavController().navigate(InstructionsFragmentDirections.actionInstructionsFragmentToShoeListFragment())
         }
         return binding.root
     }
